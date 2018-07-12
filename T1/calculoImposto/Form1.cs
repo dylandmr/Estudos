a9ad0a82c06bc16fc4b9154a8e24b6c40079cc1c
v007 <v007@benner.com.br>
2018-07-12 14:49:39 -0300

    Aula 3: Novo Projeto "calculoImposto"

---
+++ T1/calculoImposto/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculoImposto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double valorDaNotaFiscal = 1000;
+
+            if (valorDaNotaFiscal < 1000) MessageBox.Show("Imposto de 2%. Valor final = " + (valorDaNotaFiscal * 1.02));
+            else if ((valorDaNotaFiscal >= 1000) && (valorDaNotaFiscal < 3000)) MessageBox.Show("Imposto de 2,5%. Valor final = " + (valorDaNotaFiscal * 1.025));
+            else if ((valorDaNotaFiscal >= 3000) && (valorDaNotaFiscal < 7000)) MessageBox.Show("Imposto de 2,8%. Valor final = " + (valorDaNotaFiscal * 1.028));
+            else if (valorDaNotaFiscal >= 7000) MessageBox.Show("Imposto de 3%. Valor final = " + (valorDaNotaFiscal * 1.03));
+        }
+    }
+}
