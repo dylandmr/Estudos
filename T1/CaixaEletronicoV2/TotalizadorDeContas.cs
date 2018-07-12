3492fd9cb9af721ab4daefc0015566e92860994c
v007 <v007@benner.com.br>
2018-07-12 16:45:09 -0300

    Teste classe TotalizadorDeContas: Erro de Acessibilidade Inconsistente (provocado propositalmente).

---
+++ T1/CaixaEletronicoV2/TotalizadorDeContas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronicoV2
+{
+    public class TotalizadorDeContas
+    {
+        public double Saldo { get; private set; }
+        public void Adiciona(Conta c)
+        {
+            this.Saldo = c.Saldo;
+        }
+    }
+}
