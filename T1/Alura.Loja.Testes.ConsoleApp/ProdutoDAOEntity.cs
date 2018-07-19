7d8d7a00f5a263b2427ee1cad4fca4906d0a8a92
v007 <v007@benner.com.br>
2018-07-19 17:30:15 -0300

    Aula 2 - Criando Interface IProdutoDAO e ProdutoDAOEntity.

---
+++ T1/Alura.Loja.Testes.ConsoleApp/ProdutoDAOEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class ProdutoDAOEntity : IProdutoDAO
+    {
+        public void Adicionar(Produto p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Produto p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Produto> Produtos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(Produto p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
