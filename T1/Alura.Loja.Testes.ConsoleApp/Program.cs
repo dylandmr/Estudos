f56a0f57228990d80a5d9667bc3f86cde7cd64ac
v007 <v007@benner.com.br>
2018-07-23 14:13:01 -0300

    Aula 4 - Migrations: Limpando Main, alterando propriedade Preço e criando nova propriedade Unidade na classe Produto.

--- T1/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T1/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -14,55 +14,7 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            using (var contexto = new LojaContext())
-            {
-                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
-                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
-                loggerFactory.AddProvider(SqlLoggerProvider.Create());
-
-                var produtos = contexto.Produtos.ToList();
-
-                ImprimeChangeTracker(contexto.ChangeTracker.Entries());
-
-                //var p = contexto.Produtos.ToList().First();
-
-                //Console.Write("Alterar nome: ");
-                //p.Nome = Console.ReadLine();
-
-                //ImprimeChangeTracker(contexto.ChangeTracker.Entries());
-
-                //contexto.SaveChanges();
-
-                Console.WriteLine("Novo Produto - Nome [ENTER] - Preço [ENTER] - Categoria [ENTER] ");
-                var novoP = new Produto()
-                {
-                    Nome = Console.ReadLine(),
-                    Preco = Convert.ToDouble(Console.ReadLine()),
-                    Categoria = Console.ReadLine()
-                };
-
-                contexto.Produtos.Add(novoP);
-                contexto.Produtos.Remove(novoP);
-
-                var entry = contexto.Entry(novoP);
-                Console.WriteLine("\n\n---\n" + entry.Entity.ToString() + " - " + entry.State + "\n---\n\n");
-
-                ImprimeChangeTracker(contexto.ChangeTracker.Entries());
-
-                contexto.SaveChanges();
-
-                ImprimeChangeTracker(contexto.ChangeTracker.Entries());
-            }
-
-            void ImprimeChangeTracker(IEnumerable<EntityEntry> entries)
-            {
-                Console.WriteLine("------------------------");
-                foreach (var e in entries)
-                {
-                    Console.WriteLine(e.Entity.ToString() + " - " +e.State);
-                }
-                Console.WriteLine("------------------------");
-            }
+            
         }        
     }
 }
