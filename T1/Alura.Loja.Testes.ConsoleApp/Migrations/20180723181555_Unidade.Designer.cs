8646dc8c7f403c81a6680c32248d3e726a1c9a75
v007 <v007@benner.com.br>
2018-07-23 15:16:51 -0300

    Aula 4 - Migrations: Refazendo alterações na classe Produto e adicionando novamente Migração 'Unidade'.

--- T1/Alura.Loja.Testes.ConsoleApp/Migrations/20180723181555_Unidade.Designer.cs
+++ T1/Alura.Loja.Testes.ConsoleApp/Migrations/20180723181555_Unidade.Designer.cs
@@ -8,9 +8,10 @@ using Alura.Loja.Testes.ConsoleApp;
 namespace Alura.Loja.Testes.ConsoleApp.Migrations
 {
     [DbContext(typeof(LojaContext))]
-    partial class LojaContextModelSnapshot : ModelSnapshot
+    [Migration("20180723181555_Unidade")]
+    partial class Unidade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
