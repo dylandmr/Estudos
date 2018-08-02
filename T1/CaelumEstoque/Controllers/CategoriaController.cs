7a3fcd7d5a8de8091a498341a78eecd928e5c888
dylandmr <dylandmrodrigues@hotmail.com>
2018-08-02 09:58:55 -0300

    Curso ASP.NET MVC5 - Aula 3: Exercício - Criando CategoriaController e View Index que lista as categorias cadastradas no banco.

---
+++ T1/CaelumEstoque/Controllers/CategoriaController.cs
@@ -0,0 +1,21 @@
+﻿using CaelumEstoque.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            var categoriaDAO = new CategoriasDAO();
+            var categorias = categoriaDAO.Lista();
+            ViewBag.Categorias = categorias;
+            return View();
+        }
+    }
+}
