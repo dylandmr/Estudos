bf383d7f80e22f98c127b2278cec674d95c5888d
v007 <v007@benner.com.br>
2018-08-03 17:55:19 -0300

    Curso ASP.NET MVC5 - Aula 10: Criando contador de acessos via Session e utilizando Session para criar LoginController e tela de Login no Index.

---
+++ T1/CaelumEstoque/Controllers/ContadorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ContadorController : Controller
+    {        
+        public ActionResult Index()
+        {
+            int contador = Convert.ToInt32(Session["contador"]);
+            Session["contador"] = ++contador;
+            return View(contador);
+        }
+    }
+}
---
+++ T1/CaelumEstoque/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using CaelumEstoque.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Autentica(String email, String senha)
+        {
+            var usuariosDAO = new UsuariosDAO();
+            var usuario = usuariosDAO.Busca(email, senha);
+            if (usuario != null)
+            {
+                Session["usuarioLogado"] = usuario;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
