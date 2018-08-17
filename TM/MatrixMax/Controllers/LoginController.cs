5a35ea8b6bfafeea4331543cad0700952d889db3
dylandmr <dylandmrodrigues@hotmail.com>
2018-08-17 04:08:53 -0300

    Projeto Banca - Criando Página de Login e implementando validação e filtro de autentificação.

---
+++ TM/MatrixMax/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using MatrixMax.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MatrixMax.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autentica(string login, string senha)
+        {
+            var usuario = new UsuarioDAO().Autentica(login, senha);
+            if (usuario != null)
+            { 
+                Session["usuarioLogado"] = usuario;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("CredenciaisIncorretas", "Login e/ou senha inválido(s)!");
+                return View("Index");
+            }
+        }
+    }
+}
