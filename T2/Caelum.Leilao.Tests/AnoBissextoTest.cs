cf889a9c8433e92591879b023b9d9b563365abd0
dylandmr <dylandmrodrigues@hotmail.com>
2018-08-24 09:27:01 -0300

    TDD com C# - Aula 3: Implementando exercício Ano Bissexto.

---
+++ T2/Caelum.Leilao.Tests/AnoBissextoTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caelum.Leilao
+{
+    [TestFixture]
+    public class AnoBissextoTest
+    {
+        [Test]
+        public void DeveRetornarQueO2020SeraBissexto()
+        {
+            Assert.IsTrue(new AnoBissexto().EhBissexto(2020));
+        }
+
+        [Test]
+        public void DeveRetornarQueO2019NaoSeraBissexto()
+        {
+            Assert.IsFalse(new AnoBissexto().EhBissexto(2018));
+        }
+
+        [Test]
+        public void DeveRetornarQueO2016FoiBissexto()
+        {
+            Assert.IsTrue(new AnoBissexto().EhBissexto(2020));
+        }
+
+        [Test]
+        public void DeveRetornarQueO2017NaoFoiBissexto()
+        {
+            Assert.IsFalse(new AnoBissexto().EhBissexto(2018));
+        }
+    }
+}
--- T2/Caelum.Leilao.Tests/FiltroDeLancesTest.cs
+++ T2/Caelum.Leilao.Tests/FiltroDeLancesTest.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Caelum.Leilao.Tests
+namespace Caelum.Leilao
 {
     [TestFixture]
     public class FiltroDeLancesTest
