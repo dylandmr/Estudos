1f4b18d4669c874914c023014fec03386790ce9a
dylandmr <dylandmrodrigues@hotmail.com>
2018-08-24 17:31:31 -0300

    Curso C# Reflection parte 1 - Aula 1: Criando projeto ByteBank e começando a implementar códigos da aula.

---
+++ T2/ByteBank.Portal/Infraestrutura/WebApplication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Portal.Infraestrutura
+{
+    public class WebApplication
+    {
+        private readonly string[] _prefixos;
+
+        public WebApplication(string[] prefixos)
+        {
+            if (prefixos == null) throw new ArgumentNullException(nameof(prefixos));
+            _prefixos = prefixos;
+        }
+
+        public void Iniciar()
+        {
+            var httpListener = new HttpListener();
+
+            foreach (var prefixo in _prefixos) httpListener.Prefixes.Add(prefixo);
+
+            httpListener.Start();
+
+        }
+    }
+}
