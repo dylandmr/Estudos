60f0487aeb62c345c1238f125fc33ffc107f9404
dylandmr <dylandmrodrigues@hotmail.com>
2018-08-27 11:57:03 -0300

    C# Reflection - Aula 1: Criando HttpListener para lidar com as requisições do Localhost; Fazendo o localhost retornar 'Hello world'; Adicionando recursos ao Assembly; Buscando recursos do Assembly em execução via Reflection e criando rota para retornar os arquivos main.js e styles.css no navegador via localhost.

--- T2/ByteBank.Portal/Infraestrutura/WebApplication.cs
+++ T2/ByteBank.Portal/Infraestrutura/WebApplication.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,18 +14,71 @@ namespace ByteBank.Portal.Infraestrutura
 
         public WebApplication(string[] prefixos)
         {
-            if (prefixos == null) throw new ArgumentNullException(nameof(prefixos));
-            _prefixos = prefixos;
+            _prefixos = prefixos ?? throw new ArgumentNullException(nameof(prefixos));
         }
 
         public void Iniciar()
         {
+            while (true)
+                ManipularRequisicao();
+        }
+
+        private void ManipularRequisicao()
+        {
             var httpListener = new HttpListener();
 
-            foreach (var prefixo in _prefixos) httpListener.Prefixes.Add(prefixo);
+            foreach (var prefixo in _prefixos)
+                httpListener.Prefixes.Add(prefixo);
 
             httpListener.Start();
 
+            var contexto = httpListener.GetContext();
+            var requisicao = contexto.Request;
+            var resposta = contexto.Response;
+            var path = requisicao.Url.AbsolutePath;
+            var assembly = Assembly.GetExecutingAssembly();
+
+            switch (path)
+            {
+                case "/Assets/css/styles.css":
+                    //Retorna o styles.css
+                    var resourceStream = assembly.GetManifestResourceStream("ByteBank.Portal.Assets.css.styles.css");
+                    var bytesResource = new byte[resourceStream.Length];
+
+                    resourceStream.Read(bytesResource, 0, (int)resourceStream.Length);
+
+                    resposta.ContentType = "text/css; charset=utf-8";
+
+                    resposta.StatusCode = 200;
+
+                    resposta.ContentLength64 = resourceStream.Length;
+
+                    resposta.OutputStream.Write(bytesResource, 0, bytesResource.Length);
+
+                    resposta.OutputStream.Close();
+
+                    break;
+                case "/Assets/js/main.js":
+                    //Retorna o main.js
+                    resourceStream = assembly.GetManifestResourceStream("ByteBank.Portal.Assets.js.main.js");
+                    bytesResource = new byte[resourceStream.Length];
+
+                    resourceStream.Read(bytesResource, 0, (int)resourceStream.Length);
+
+                    resposta.ContentType = "application/js; charset=utf-8";
+
+                    resposta.StatusCode = 200;
+
+                    resposta.ContentLength64 = resourceStream.Length;
+
+                    resposta.OutputStream.Write(bytesResource, 0, bytesResource.Length);
+
+                    resposta.OutputStream.Close();
+
+                    break;
+            }
+
+            httpListener.Stop();
         }
     }
 }
