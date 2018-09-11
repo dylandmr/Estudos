38b855f18ffdd7f9bf601ccfd5b5012b7d051935
dylandmr <dylandmrodrigues@hotmail.com>
2018-09-11 09:27:31 -0300

    Curso Javascript - Aula 3: Utilizando querySelectorAll() e for para calcular IMCs de toda a tabela em loop e adicionando classes e estilos às colunas inválidas via classList.add().

--- T3/introducao-javascript/js/principal.js
+++ T3/introducao-javascript/js/principal.js
@@ -6,41 +6,40 @@ console.log(titulo.textContent);
 titulo.textContent = "Aparecida Nutricionista";
 //erro
 
-//var pacientes = document.querySelectorAll(".paciente");
-//var altura = 0; var peso = 0;
-//for (var i = 0; i < pacientes.length; i++ ){
-//    peso = pacientes.item(i).querySelector(".info-peso").textContent;
-//    altura = pacientes.item(i).querySelector(".info-altura").textContent;
-//    pacientes.item(i).querySelector(".info-imc").textContent = peso/(altura*altura);
-//}
+var pacientes = document.querySelectorAll(".paciente");
+for (var i = 0; i < pacientes.length; i++ ){
+    var paciente = pacientes[i];
+    
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var paciente = document.querySelector("#primeiro-paciente");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+    var tdImc = paciente.querySelector(".info-imc");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var pesoValido = true;
+    var alturaValida = true;
 
-var tdImc = paciente.querySelector(".info-imc");
+    if(peso < 0 || peso > 500 ){
+        //paciente.style.backgroundColor = "lightcoral";
+        paciente.classList.add("paciente-invalido");
+        tdPeso.textContent = "Peso inválido."; 
+        pesoValido = false;
+    }
 
-var pesoValido = true;
-var alturaValida = true;
+    if(altura < 0 || altura > 3){
+        //paciente.style.backgroundColor = "lightcoral";
+        paciente.classList.add("paciente-invalido");
+        tdAltura.textContent = "Altura inválida.";
+        alturaValida = false;
+    }
 
-if(peso < 0 || peso > 500 ){
-    tdPeso.textContent = "Peso inválido."; 
-    pesoValido = false;
-}
+    if(pesoValido && alturaValida) {
+        var imc = peso/(altura*altura); // 100/(2.00*2.00) // 100/4.00 = 25
 
-if(altura < 0 || altura > 3){
-    tdAltura.textContent = "Altura inválida.";
-    alturaValida = false;
-}
-
-if(pesoValido && alturaValida) {
-    var imc = peso/(altura*altura); // 100/(2.00*2.00) // 100/4.00 = 25
-
-    tdImc.textContent = imc;
-} else {
-    tdImc.textContent = "Altura e/ou peso inválido(s)!"
+        tdImc.textContent = imc.toFixed(2);
+    } else {
+        tdImc.textContent = "Altura e/ou peso inválido(s)!"
+    }
 }
