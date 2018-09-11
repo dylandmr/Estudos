ff519448bc00e45877ea94da1149d76d8f37901e
dylandmr <dylandmrodrigues@hotmail.com>
2018-09-11 10:53:00 -0300

    Curso Javascript - Aula 4: addEventListener(), preventDefault(), createElement(), appendChild().

--- T3/introducao-javascript/js/principal.js
+++ T3/introducao-javascript/js/principal.js
@@ -43,3 +43,48 @@ for (var i = 0; i < pacientes.length; i++ ){
         tdImc.textContent = "Altura e/ou peso inválido(s)!"
     }
 }
+
+//titulo.addEventListener("click", mostraMensagem); <- função nomeada
+
+//function mostraMensagem() {
+//    console.log("clicou no título!");
+//}
+
+titulo.addEventListener("click", function() {
+    console.log("clicou no título ;)");
+}); //<- FUNÇÃO ANÔNIMA
+
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+    
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+    
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    
+    var tabela = document.querySelector("#tabela-pacientes");
+    
+    tabela.appendChild(pacienteTr);
+});
