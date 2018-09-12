656db5192c279fdd39c0af4e2b76a861be2fde56
dylandmr <dylandmrodrigues@hotmail.com>
2018-09-12 09:16:40 -0300

    Aula 8 - Filtrando a tabela com o evento input; Introdução à palavra reservada 'this' dentro de métodos; Aprofundando filtro com expressões regulares -> new RegExp() e RegExp.test().

--- T3/introducao-javascript/js/filtra.js
+++ T3/introducao-javascript/js/filtra.js
@@ -1,15 +1,22 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function() {
-    console.log(this.value);
     var pacientes = document.querySelectorAll(".paciente");
-    pacientes.forEach(function(paciente) {
-        var tdNome = paciente.querySelector(".info-nome");
-        nome = tdNome.textContent;
-        if (nome != this.value){
-            paciente.classList.add(".invisivel");
-        } else {
-            paciente.classList.remove(".invisivel");
-        }
-    });
+    if (this.value.length > 0) {
+        pacientes.forEach(function(paciente) {
+            var tdNome = paciente.querySelector(".info-nome");
+            nome = tdNome.textContent;
+            var expressao = new RegExp(campoFiltro.value, "i");
+            console.log(nome);
+            if (!expressao.test(nome)){
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        });
+    } else {
+        pacientes.forEach(function(paciente) {
+            paciente.classList.remove("invisivel");
+        });        
+    }
 });
