b509534cdd23525d0776bc2b80ca1e565320d3df
dylandmr <dylandmrodrigues@hotmail.com>
2018-09-14 00:41:16 -0300

    Aula 3 - Eventos: .on('focus', attr('disabled', true/false), setInterval(acao,tempo), clearInterval(intervalo), .one(evento, acao); Aula 4: Shorthand eventos: .click(acao), .focus(acao), .ready(acoes); => , removeAttr('atributo').

--- T3/alura-typer/public/js/main.js
+++ T3/alura-typer/public/js/main.js
@@ -1,12 +1,48 @@
-var frase = $(".frase").text();
-
-var contPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase").text(contPalavras);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
 
-campo.on("input", function(){
-    $("#contador-palavras").text(campo.val().split(/\S+/).length-1);
-    $("#contador-caracteres").text(campo.val().replace(/\s+/g, "").length);
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#tempo-digitacao").text(tempoInicial);
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    inicializaCronometro();
+}
+
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var contPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase").text(contPalavras);
+}
+
+function inicializaContadores() {
+    campo.on("input", function () {
+        $("#contador-palavras").text(campo.val().split(/\S+/).length - 1);
+        $("#contador-caracteres").text(campo.val().replace(/\s+/g, "").length);
+    });
+}
+
+function inicializaCronometro() {
+    $("#botao-reiniciar").attr("disabled", true);
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function () {
+        var contador = setInterval(function () {
+            tempoRestante--;
+            console.log(tempoRestante);
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(contador);
+                $("#botao-reiniciar").removeAttr("disabled");
+            }
+        }, 1000);
+    });
+}
