8990df78342751039408c372e2824b2dbe08dc97
dylandmr <dylandmrodrigues@hotmail.com>
2018-10-14 13:38:10 -0300

    Curso C# Refatoração 1 - Arquivos Aula 1-8 - Fim de curso; Importando Projetos Curso C# Refatoração 2.

---
+++ TC/refatoracao/Aula07/R14.HideDelegate/antes/Escola.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R14.HideDelegate.antes
+{
+    class Escola
+    {
+        public string Nome { get; private set; }
+        public Funcionario Diretor { get; private set; }
+    }
+
+    class Departamento
+    {
+        public Escola Escola { get; private set; }
+    }
+
+    class Funcionario
+    {
+        private Departamento Departamento { get; }
+
+        public Funcionario GetDiretor()
+        {
+            return GetEscola().Diretor;
+        }
+
+        public Escola GetEscola()
+        {
+            return Departamento.Escola;
+        }
+    }
+
+    class Teste
+    {
+        public Teste()
+        {
+            var maria = new Funcionario();
+            var diretorDaMaria = maria.GetDiretor();
+        }
+    }
+}
---
+++ TC/refatoracao/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+
+namespace refatoracao
+{
+    class Program
+    {
+        private int redimensionado;
+        private string plataforma;
+        private string browser;
+
+        static void Main(string[] args)
+        {
+        }
+
+        void GerarListagem()
+        {
+            var plataformaMac = plataforma.ToUpper().IndexOf("MAC") > -1;
+            var navegadorChrome = browser.ToUpper().IndexOf("Chrome") > -1;
+            var foiRedimensionado = redimensionado > 0;
+            if (plataformaMac 
+                && navegadorChrome 
+                && foiInicializado() 
+                && foiRedimensionado)
+            {
+                // faça alguma coisa
+            }
+        }
+
+        class CalculadoraDePrecos
+        {
+            decimal GetDescontoFinal(decimal descontoInicial
+            , int quantidade
+            , int clienteHaQuantosAnos)
+            {
+                var resultado = descontoInicial;
+                if (resultado > 50M)
+                {
+                    resultado = 50M;
+                }
+                if (quantidade > 100)
+                {
+                    resultado += 15M;
+                }
+                if (clienteHaQuantosAnos > 4)
+                {
+                    resultado += 10M;
+                }
+                return resultado;
+            }
+        }
+
+        private bool foiInicializado()
+        {
+            throw new NotImplementedException();
+        }
+
+        class Produto
+        {
+            private readonly string descricao;
+            private bool promocional;
+            private readonly decimal precoBase;
+            private readonly decimal acrescimo;
+            private readonly decimal desconto;
+
+            public string Descricao { get => descricao; }
+            public bool Promocional { get => promocional; }
+            public decimal PrecoBase { get => precoBase; }
+            public decimal Acrescimo { get => acrescimo; }
+            public decimal Desconto { get => desconto; }
+
+            public Produto(string descricao, decimal precoBase, decimal acrescimo, decimal desconto)
+            {
+                this.descricao = descricao;
+                this.precoBase = precoBase;
+                this.acrescimo = acrescimo;
+                this.desconto = desconto;
+
+                var preco = Preco(precoBase, acrescimo, desconto);
+
+                Console.WriteLine($"ANTES: O preço é {preco}");
+            }
+
+            decimal Preco(decimal precoBase, decimal acrescimo, decimal desconto)
+            {
+                return new GeradoraDePrecos(this, precoBase, acrescimo, desconto).Calcula();
+            }
+
+            public void HabilitarPromocao()
+            {
+                if (desconto == 0)
+                {
+                    this.promocional = true;
+                }
+                else
+                {
+                    throw new Exception("Produto com desconto não pode ser transformado em produto promocional!");
+                }
+            }
+        }
+
+        class GeradoraDePrecos
+        {
+            private readonly Produto produto;
+            private decimal precoBase;
+            private decimal acrescimo;
+            private decimal desconto;
+
+            public GeradoraDePrecos(Produto produto, decimal precoBase, decimal acrescimo, decimal desconto)
+            {
+                this.produto = produto;
+                this.precoBase = precoBase;
+                this.acrescimo = acrescimo;
+                this.desconto = desconto;
+            }
+
+            public decimal Calcula()
+            {
+                var resultado = precoBase;
+
+                if (produto.Promocional && desconto > 0)
+                {
+                    throw new Exception("Produto já é promocional e não pode ter desconto!");
+                }
+
+                if (desconto > 20)
+                {
+                    desconto = 20;
+                }
+
+                if (acrescimo > 15)
+                {
+                    acrescimo = 15;
+                }
+
+                return precoBase + precoBase * (acrescimo - desconto);
+            }
+        }
+    }
+
+    class ContaCorrente
+    {
+        public decimal GetTaxaChequeEspecial()
+        {
+            return tipo.GetTaxaChequeEspecial(this);
+        }
+
+        public ContaCorrente(TipoConta type)
+        {
+            this.tipo = type;
+        }
+
+        private readonly TipoConta tipo;
+        public TipoConta Tipo { get { return tipo; } }
+
+        private int diasEmDescoberto;
+        public int DiasEmDescoberto
+        {
+            get { return diasEmDescoberto; }
+        }
+    }
+
+    class TipoConta
+    {
+        public bool EhPremium { get; set; }
+
+        public decimal GetTaxaChequeEspecial(ContaCorrente conta)
+        {
+            if (EhPremium)
+            {
+                var result = 10.0M;
+                if (conta.DiasEmDescoberto > 7)
+                {
+                    result += (conta.DiasEmDescoberto - 7) * 0.01M;
+                }
+                return result;
+            }
+
+            return conta.DiasEmDescoberto * 1.75M;
+        }
+    }
+}
